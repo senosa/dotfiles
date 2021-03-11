f9332947f2b98ee400fcd0f2b1edb294938def68
Sensuke Osawa <senosa@gmail.com>
2021-03-11 14:52:13 +0900

    Add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -eu
+
+DOTPATH=$HOME/dotfiles
+
+BRANCH="${1:-master}"
+echo "Bootstrap with branch '${BRANCH}'"
+
+if [ ! -d "$DOTPATH" ]; then
+  git clone -b "$BRANCH" https://github.com/senosa/dotfiles.git "$DOTPATH"
+else
+  echo "$DOTPATH already downloaded. Updating..."
+  cd "$DOTPATH"
+  git stash
+  git checkout "$BRANCH"
+  git pull origin "$BRANCH"
+  echo
+fi
+
+cd "$DOTPATH"
+
+# ./bootstrap/main.sh
+
+echo "Bootstrapping DONE!"
