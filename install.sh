aed2d12d965f2bef611ddf187fcbb2adf792a6f0
Sensuke Osawa <senosa@gmail.com>
2022-03-29 13:37:54 +0900

    Clean up homebrew set up

--- install.sh
+++ install.sh
@@ -25,42 +25,11 @@ echo 'Linking dotfiles'
 scripts/deploy.sh
 echo
 
-command_prefix=""
-if [ "$(uname -m)" = "arm64" ]; then
-  # Install on Rosetta 2
-  command_prefix="arch -arch x86_64"
+echo 'Setting up homebrew'
+if !(type "brew" >/dev/null 2>&1); then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
-
-# # Install Rosetta 2 when ARM
-# if [ "$(uname -m)" = "arm64" ]; then
-#   softwareupdate --install-rosetta --agree-to-license
-# fi
-
-# # Install homebrew for Intel
-# if ! command -v /usr/local/bin/brew >/dev/null 2>&1; then
-#   # Install homebrew: https://brew.sh/
-#   $command_prefix /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-# fi
-
-# # Install homebrew for ARM
-# if [ "$(uname -m)" = "arm64" ] && (! command -v /opt/homebrew/bin/brew > /dev/null); then
-#   sudo mkdir /opt/homebrew
-#   sudo chown "$(whoami)" /opt/homebrew
-#   curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C /opt/homebrew
-# fi
-
-# # TODO: prioritize ARM homebrew
-# # export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH"
-
-$command_prefix brew bundle --no-lock
+brew bundle --no-lock
 echo
-
-# mackup restore
-# echo
-
-# scripts/initialize.sh
-# echo
-
-# scripts/configure_brew.sh
 
 echo "Bootstrapping DONE!"
